a22f8d0a1b1bd2616ae1b272e3f2dc1b2312e7e8
pix <303pix@gmail.com>
2023-05-11 00:45:32 +0200

    fix config for railway

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/pix303/localemgmtES-api/router"
 )
@@ -11,7 +12,11 @@ func main() {
 	log.Println("Init api router")
 	r, _ := router.NewAPI()
 	log.Println("going to run router")
-	log.Fatalln(http.ListenAndServe(":8080", r.Router))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatalln(http.ListenAndServe("0.0.0.0:"+port, r.Router))
 	log.Println("something goes wrong")
 
 }
