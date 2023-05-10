1709ef9be2e3232fc262be6159e10d7874e15563
pix <303pix@gmail.com>
2023-05-11 00:40:08 +0200

    add init logs

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	log.Println("Init api router")
 	r, _ := router.NewAPI()
+	log.Println("going to run router")
 	log.Fatalln(http.ListenAndServe(":8080", r.Router))
+	log.Println("something goes wrong")
 
 }
